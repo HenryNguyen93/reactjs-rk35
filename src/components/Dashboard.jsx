bd7743a3619ef11f53bae39e2cd6a0be61703671
Henry Nguyen <hanhuy.cvp@gmail.com>
2023-10-16 17:09:53 +0700

    add again

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import './DashBoard.css'
+import "../components/Dashboard.css"
 import {
     HomeOutlined,
     LoadingOutlined,
